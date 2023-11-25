60878ca18581fad6d4028b84d10390c376753cf1
HARSHENDRARAMI <144218834+HARSHENDRARAMI@users.noreply.github.com>
2023-11-25 09:39:59 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.mongodb+srv://hrcreates2021:MongoC0@cluster0.y2xcbrd.mongodb.net/?retryWrites=true&w=majority);
+    const conn = await mongoose.connect('mongodb+srv://hrcreates2021:MongoC0@cluster0.y2xcbrd.mongodb.net/?retryWrites=true&w=majority');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
